74618e1b60505f0bf5285558ae9e51f4c7c4ae71
Mohamed Taher <mo.taher717@gmail.com>
2024-09-27 01:07:08 +0300

    update for checker

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -60,6 +60,7 @@ class ProfileView(generics.RetrieveUpdateAPIView):
 
 
 class FollowUserView(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()
     permission_classes = [IsAuthenticated]
     serializer_class = FollowUserSerializer
 
